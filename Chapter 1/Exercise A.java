cca8926be788b604b3af6a71eba40e64eb1a1f91
hilarysamanthaJosol <hilarysamanthajosol@gmail.com>
2024-08-15 21:24:18 +0800

    Add files via upload

---
+++ Chapter 1/Exercise A.java
@@ -0,0 +1,13 @@
+
+ public class TableAndChairs
+ {
+	public static void main(String[]args) 
+	{
+		System.out.println("X                                   X");
+		System.out.println("X                                   X");
+		System.out.println("X           XXXXXXXXXXXX            X");
+		System.out.println("XXXXXXXXX    X        X     XXXXXXXXx");
+		System.out.println("X      X    X          X     X      X");
+		System.out.println("X      X    X          X     X      X");
+	}
+ }
