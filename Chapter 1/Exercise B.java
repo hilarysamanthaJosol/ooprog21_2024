d9f7d24befb2f1912eb96017ae7b95d48aca8381
hilarysamanthaJosol <hilarysamanthajosol@gmail.com>
2024-08-15 21:48:29 +0800

    Add files via upload

---
+++ Chapter 1/Exercise B.java
@@ -0,0 +1,13 @@
+
+ public class MovieQuoteInfo
+ {
+	public static void main(String[]args) 
+	{
+        String movieQuote = " Being with you is the only way I could have a full and happy life. You’re the girl of my dreams—and apparently, I’m the man of yours.";
+	    String movieProducedby  = " Produced by Sony Pictures Entertainment ";
+	    String movieCharacter = " Adam Sandler plays Henry";
+	    String movieYearMade = " 2004 ";
+	    
+	    System.out.print("\n" + movieQuote + "\n" + movieProducedby +"\n" + movieCharacter + "\n" + movieYearMade);
+	}
+ }
